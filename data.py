19ef1ad097d1668a8269097c0111baba8867b326
SabaPivot <careforme.dropout@gmail.com>
2024-12-02 17:49:22 +0900

    Fix: remove redundant code

--- data.py
+++ data.py
@@ -6,25 +6,20 @@ login(token="hf_token")
 
 def prepare_datasets(args):
     datasets = load_dataset(args.data_path)
-    print(datasets)
     train_dataset = datasets["train"]
     val_dataset = datasets["validation"]
     test_dataset = datasets["test"]
-    print(load_dataset("json" ,data_files="/home/careforme.dropout/original+smilegate.jsonl"))
-    augmented_train_dataset = load_dataset("json" ,data_files="/home/careforme.dropout/original+smilegate.jsonl")["train"]
 
+    # 증강한 데이터가 있다면 아래 코드 실행
+    # augmented_train_dataset = load_dataset("json" ,data_files="augmented_train_dataset.jsonl")["train"]
     tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
 
-    # add special tokens
     special_tokens_dict = {
         'additional_special_tokens': ['&name&', '&affiliation&', '&social-security-num&', '&tel-num&', '&card-num&',
                                       '&bank-account&', '&num&', '&online-account&']
     }
-    print(len(tokenizer))
     tokenizer.add_special_tokens(special_tokens_dict)
-    print(len(tokenizer))
 
-    # ToDo: truncation, max_length
     def tokenize_function(dataset):
         return tokenizer(
             dataset["input"],
@@ -37,8 +32,9 @@ def prepare_datasets(args):
         )
 
     return (
-        # train_dataset.map(tokenize_function, batched=True).rename_column("output", "labels"),
-        augmented_train_dataset.map(tokenize_function, batched=True).rename_column("output", "labels"),
+        train_dataset.map(tokenize_function, batched=True).rename_column("output", "labels"),
+        # 증강한 데이터가 있다면 아래 코드 실행
+        # augmented_train_dataset.map(tokenize_function, batched=True).rename_column("output", "labels"),
         val_dataset.map(tokenize_function, batched=True).rename_column("output", "labels"),
         test_dataset.map(tokenize_function, batched=True).rename_column("output", "labels")
     )
