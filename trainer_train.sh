bd8a47e861747f5f1c1a821d53b8809c3d828e9a
SabaPivot <careforme.dropout@gmail.com>
2024-12-02 17:10:13 +0900

    Refactor: sshell sripts

--- trainer_train.sh
+++ trainer_train.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 python main.py \
-  --mode do_train \
-  --data_path SabaPivot/hate_speech \
-  --model_name monologg/kobert \
-  --epochs 12 \
-  --batch_size 32 \
-  --lr 5e-5\
-  --warmup_steps 20 \
+  --mode \
+    do_train \
+  --data_path \
+    SabaPivot/hate_speech \
+  --model_name \
+    monologg/kobert \
+  --epochs \
+    12 \
+  --batch_size \
+    32 \
+  --lr \
+    5e-5\
+  --warmup_steps \
+    20 \
   --save_limit 1 \
-  --run_name "Augmented_1e-4_32_20_12_classifier_dropout=0.1_fp16_monologg/kobert" \
-  --data_type "Augmented" \
-  --early_stopping_patience 5 \
+  --run_name \
+    "your_run_name" \
+  --data_type \
+    "your_data_type" \
+  --early_stopping_patience \
+    5 \
