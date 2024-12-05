209b574d9d98a57cb6fa33334dfcf27dfa1690c8
SabaPivot <58300587+SabaPivot@users.noreply.github.com>
2024-12-06 02:12:36 +0900

    Fix: error in inference (#6)

--- trainer_evaluation.sh
+++ trainer_evaluation.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 python main.py \
-  --mode \
-    do_eval \
-  --data_path \
-    SabaPivot/hate_speech \
-  --model_name \
-    model1_name \
-    model2_name \
+  --mode do_eval \
+  --data_path "SabaPivot/hate_speech" \
+  --model_name team-lucid/deberta-v3-xlarge-korean team-lucid/deberta-v3-xlarge-korean team-lucid/deberta-v3-xlarge-korean \
   --model_dir \
-    model1_path \
-    model2_path \
+    1e-05_team-lucid/deberta-v3-xlarge-korean/checkpoint-1896 \
+    3e-05_team-lucid/deberta-v3-xlarge-korean/checkpoint-4424 \
+    7e-06_team-lucid/deberta-v3-xlarge-korean/checkpoint-1896
